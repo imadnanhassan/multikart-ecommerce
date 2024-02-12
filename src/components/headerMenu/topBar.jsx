9eb7a2f8011417eb0f22158df1ed8ccab18f2312
imadnanhassan <abulhassan.dev@gmail.com>
2024-02-12 15:25:12 +0600

    top header desktop view complete

--- src/components/headerMenu/topBar.jsx
+++ src/components/headerMenu/topBar.jsx
@@ -1,15 +1,25 @@
+import { IoIosCall } from "react-icons/io";
+import { FaHeart } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa";
 export default function Topbar() {
   return (
-    <div className="bg-[#2d2a25] py-3 ">
+    <div className="bg-[#2d2a25] py-3">
       <div className="flex justify-between items-center main-container text-white">
         <div className="top-contact flex items-center gap-5 ">
           <p>Welcome to Our store Multikart</p>
-          <p>Call Us: 123 - 456 - 7890</p>
+          <p className="flex items-center gap-1">
+            <IoIosCall className="text-[20px] mb-1" />
+            Call Us: 123 - 456 - 7890
+          </p>
         </div>
         <div className="tp-auth flex items-center">
           <ul className="flex gap-5">
-            <li> Wishlist</li>
-            <li> My Account</li>
+            <li className="flex items-center gap-2">
+              <FaHeart className="text-[16px] mt-1" /> Wishlist
+            </li>
+            <li className="flex items-center gap-2">
+              <FaUser className="text-[15px] mt-1" /> My Account
+            </li>
           </ul>
         </div>
       </div>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts";
 import NotFound from "../shared/NotFound";
-import HomePage from "../pages/Home";
+import HomePage from "../frontend/pages/Home";
 
 const route = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     errorElement: <NotFound />,
-        children: [{ path: "/", element: <HomePage /> },
+        children: [{ path: "/", element: <HomePage/> },
         {path:"/about", element: <h1>About</h1>}
         ],
   },
